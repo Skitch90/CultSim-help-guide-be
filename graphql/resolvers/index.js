13a47637f3bc46c900891c1d4b266cfecafb6dd0
alesc <ale.schio90@gmail.com>
2020-10-17 12:52:52 +0200

    Clean graphql schema from no longer used types and queries

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,10 +1,5 @@
 const _ = require('lodash');
 
-const howToObtainLoreCypherMap = new Map([
-    ["Lore", "MATCH p= (:Lore)-[:UPGRADES_TO*1..]->(:Lore{name: $loreName}) RETURN 'Lore' AS fromType, nodes(p) AS path, length(p) AS pathLength"],
-    ["Book", "MATCH p= (:Book)-[:STUDY_RESULTS_IN]->(:Lore)-[:UPGRADES_TO*0..]->(:Lore{name: $loreName}) RETURN 'Book' AS fromType, nodes(p) AS path, length(p) AS pathLength"]
-]);
-
 const resolvers = {
     Query: {
         entityWithName: async (root, args, context) => {
@@ -47,19 +42,6 @@ const resolvers = {
                 }
             });
             return finalResult;
-        },
-        howToObtainLore: (root, args, context) => {
-            const { fromType } = args;
-            const session = context.driver.session();
-            const query = fromType ? howToObtainLoreCypherMap.get(fromType) : [...howToObtainLoreCypherMap.values()].join(" UNION ");
-            console.log(query);
-            session.run(query, args)
-                .then(result => result.records.forEach(record => console.log(record.get("fromType"), record.get("path"), record.get("pathLength"))));
-            return {
-                fromType: "String!",
-                path: [],
-                pathLength: 1
-            };
         }
     },
     Mutation: {
